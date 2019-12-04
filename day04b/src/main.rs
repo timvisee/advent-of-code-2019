de40cd0387764a5f792a2feb85721f796e94649d
timvisee <tim@visee.me>
2019-12-04 13:29:14 +0100

    Add day04b

--- day04b/src/main.rs
+++ day04b/src/main.rs
@@ -16,5 +16,8 @@ fn main() {
 
 fn is_valid(i: &u32) -> bool {
     let b = format!("{}", i).into_bytes();
-    b.is_sorted() && b.windows(2).any(|w| w[0] == w[1])
+    b.is_sorted() && b.windows(2).enumerate().any(|(i, w)| w[0] == w[1]
+        && *b.get(i - 1).unwrap_or(&0) != w[0]
+        && *b.get(i + 2).unwrap_or(&0) != w[0]
+    )
 }
