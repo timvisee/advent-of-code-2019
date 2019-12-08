cf01e65878aa9d9921ebf0b11f032addc258409e
timvisee <tim@visee.me>
2019-12-08 19:05:37 +0100

    Add day07b

---
+++ day07b/src/main.rs
@@ -0,0 +1,66 @@
+#![feature(slice_patterns)]
+
+use std::fs;
+use itertools::Itertools;
+
+fn main() {
+    let program: Vec<isize> = fs::read_to_string("./input.txt")
+        .unwrap()
+        .split(',')
+        .filter_map(|l| l.trim().parse::<isize>().ok())
+        .collect();
+
+    println!(
+        "Sequence: {:?}",
+        (5..10).permutations(5).max_by_key(|i| run_amplifiers(&mut program.clone(), i.to_vec())).unwrap(),
+    );
+}
+
+fn run_amplifiers(p: &mut [isize], mut i: Vec<isize>) -> isize {
+    let mut a = vec![(p.to_vec(), 0); i.len()];
+    let mut o = 0;
+    loop {
+        for (ref mut mem, ref mut pointer) in &mut a {
+            let mut s = vec![o];
+            if *pointer == 0 {
+                s.insert(0, i.pop().unwrap());
+            }
+
+            match run(mem, pointer, s) {
+                Some(s) => o = s,
+                None => return o,
+            }
+        }
+    }
+}
+
+fn run(p: &mut [isize], n: &mut usize, mut i: Vec<isize>) -> Option<isize> {
+    let get = |p: &[_], i, m| if let Some(1) = m { p[i as usize] } else { p[p[i as usize] as usize] };
+    while *n < p.len() {
+        let mut inst = format!("{}", p[*n])
+            .bytes()
+            .map(|b| b - b'0')
+            .collect::<Vec<u8>>();
+        match (inst.pop().unwrap(), inst.pop().unwrap_or(0)) {
+            (1, _) => { p[p[*n + 3] as usize] = get(p, *n + 1, inst.pop()) + get(p, *n + 2, inst.pop()); *n += 4 }
+            (2, _) => { p[p[*n + 3] as usize] = get(p, *n + 1, inst.pop()) * get(p, *n + 2, inst.pop()); *n += 4 }
+            (3, _) => { p[p[*n + 1] as usize] = i.remove(0); *n += 2 },
+            (4, _) => { *n += 2; return Some(get(p, *n - 1, inst.pop())) },
+            (5, _) if get(p, *n + 1, inst.pop()) != 0 => *n = get(p, *n + 2, inst.pop()) as usize,
+            (5, _) => *n += 3,
+            (6, _) if get(p, *n + 1, inst.pop()) == 0 => *n = get(p, *n + 2, inst.pop()) as usize,
+            (6, _) => *n += 3,
+            (7, _) => {
+                p[p[*n + 3] as usize] = if get(p, *n + 1, inst.pop()) < get(p, *n + 2, inst.pop()) { 1 } else { 0 };
+                *n += 4
+            }
+            (8, _) => {
+                p[p[*n + 3] as usize] = if get(p, *n + 1, inst.pop()) == get(p, *n + 2, inst.pop()) { 1 } else { 0 };
+                *n += 4
+            }
+            (9, 9) => return None,
+            _ => panic!("Unknown OPCODE"),
+        };
+    }
+    panic!("No output");
+}
