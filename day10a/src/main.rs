5a6d0f1a6465cb974464c5e7b0d0a48592e23392
timvisee <tim@visee.me>
2019-12-10 12:17:00 +0100

    Improve day10a

--- day10a/src/main.rs
+++ day10a/src/main.rs
@@ -1,44 +1,26 @@
 use itertools::Itertools;
 
 fn main() {
-    let map: Vec<Vec<bool>> = std::fs::read_to_string("./input.txt")
+    let astroids: Vec<(usize, usize)> = std::fs::read_to_string("./input.txt")
         .unwrap()
         .lines()
-        .map(|l| l.bytes().map(|b| b == b'#').collect::<Vec<_>>())
+        .enumerate()
+        .flat_map(|(y, l)| l.bytes().enumerate().filter(|(_, p)| p == &b'#').map(move |(x, _)| (x, y)))
         .collect();
 
     println!(
         "Max: {}",
-        (0..map[0].len())
-            .cartesian_product(0..map.len())
-            .filter(|(x, y)| map[*x][*y])
+        astroids
+            .iter()
             .map(|(x, y)| {
-                (0..map[0].len())
-                    .cartesian_product(0..map.len())
-                    .filter(|(xx, yy)| map[*xx][*yy] && !(*xx == x && *yy == y))
-                    .map(|(xx, yy)| (x as f64 - xx as f64).atan2(y as f64 - yy as f64))
-                    .map(integer_decode)
+                astroids
+                    .iter()
+                    .filter(|(xx, yy)| xx != x || yy != y)
+                    .map(|(xx, yy)| -((*x as f64 - *xx as f64).atan2(*y as f64 - *yy as f64) * 1000.0) as isize)
                     .unique()
                     .count()
             })
             .max()
             .unwrap(),
     );
-}
-
-#[derive(Clone, Hash, Eq, PartialEq)]
-struct Parts(u64, i16, i8);
-
-fn integer_decode(val: f64) -> Parts {
-    let bits: u64 = unsafe { std::mem::transmute(val) };
-    let sign: i8 = if bits >> 63 == 0 { 1 } else { -1 };
-    let mut exponent = ((bits >> 52) & 0x7ff) as i16;
-    let mantissa = if exponent == 0 {
-        (bits & 0xfffffffffffff) << 1
-    } else {
-        (bits & 0xfffffffffffff) | 0x10000000000000
-    };
-
-    exponent -= 1023 + 52;
-    Parts(mantissa, exponent, sign)
 }
