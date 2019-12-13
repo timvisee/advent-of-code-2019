865bed2acf01c2780261f7c0831a005391147fa8
timvisee <tim@visee.me>
2019-12-13 12:39:47 +0100

    Add day12a

---
+++ day12a/src/main.rs
@@ -0,0 +1,31 @@
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>").unwrap();
+    let mut pos: Vec<(isize, isize, isize)> = std::fs::read_to_string("./input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| re.captures(l).unwrap())
+        .map(|c| (c[1].parse().unwrap(), c[2].parse().unwrap(), c[3].parse().unwrap()))
+        .collect();
+    let mut vel = vec![(0, 0, 0); pos.len()];
+
+    for _ in 0..1000 {
+        vel.iter_mut().enumerate().for_each(|(i, v)| {
+            pos[0..i].iter().chain(pos[i..].iter().skip(1)).for_each(|o| {
+                v.0 += (o.0 - pos[i].0).min(1).max(-1);
+                v.1 += (o.1 - pos[i].1).min(1).max(-1);
+                v.2 += (o.2 - pos[i].2).min(1).max(-1);
+            });
+        });
+        pos.iter_mut().zip(&vel).for_each(|(p, v)| { p.0 += v.0; p.1 += v.1; p.2 += v.2 });
+    }
+
+    println!(
+        "Energy: {}",
+        pos.iter()
+            .zip(&vel)
+            .map(|(p, v)| (p.0.abs() + p.1.abs() + p.2.abs()) * (v.0.abs() + v.1.abs() + v.2.abs()))
+            .sum::<isize>()
+    );
+}
